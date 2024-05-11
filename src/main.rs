4fdd546acd1c1749c96cea61075b1b5d6b7df766
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2024-05-11 19:43:57 +0100

    feat: initialised LALRPOP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(wacc);
+
+#[test]
+fn basic_int() {
+    assert!(wacc::TermParser::new().parse("22").is_ok());
+}
+
 fn main() {
+    println!("{}", wacc::TermParser::new().parse("22").is_ok());
     println!("Hello, world!");
 }
